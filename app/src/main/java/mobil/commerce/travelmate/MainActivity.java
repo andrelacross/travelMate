2ee8e6e196c625ccea923079bba16f5f2b3107a3
Andre Lüers <andre@linux-a3y8.suse>
2017-12-04 14:07:12 +0100

    läuft

---
+++ app/src/main/java/mobil/commerce/travelmate/MainActivity.java
@@ -0,0 +1,13 @@
+package mobil.commerce.travelmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
