1ad4a5520f02751052f4cbfc878d2fd88fd57e92
Ecke1612 <eikedreyer@gmx.de>
2017-12-11 18:59:29 +0100

    Mates Activity - ListView mit Beispieldaten gefüllt - MateObjekt Klasse erstellt um Freundesdaten zu speichern und auszugeben.

---
+++ app/src/main/java/mobil/commerce/travelmate/MateObject.java
@@ -0,0 +1,32 @@
+package mobil.commerce.travelmate;
+
+/**
+ * Created by eiked on 11.12.2017.
+ */
+
+public class MateObject {
+
+    private String name;
+    private String contact;
+
+    public MateObject(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
--- app/src/main/java/mobil/commerce/travelmate/Mates.java
+++ app/src/main/java/mobil/commerce/travelmate/Mates.java
@@ -2,6 +2,15 @@ package mobil.commerce.travelmate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by andre on 04.12.17.
@@ -9,10 +18,42 @@ import android.os.Bundle;
 
 public class Mates extends Activity{
 
+    private ArrayList<MateObject> mates = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mates);
+        loadMates();
+        init();
+    }
+
+    public void init() {
+        ListView matesListView = (ListView) findViewById(R.id.mates_listView);
+        ImageButton btn_add = findViewById(R.id.btn_mate_add);
+
+        String[] mateNames = new String[mates.size()];
+        for(int i = 0; i < mates.size(); i++){
+            mateNames[i] = mates.get(i).getName();
+            System.out.println(mateNames[i]);
+        }
+        ListAdapter matesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mateNames);
+        matesListView.setAdapter(matesAdapter);
+
+        matesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String contact = mates.get(i).getContact();
+                Toast.makeText(Mates.this, contact, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
+
+    private void loadMates() {
+        mates.add(new MateObject("Name1", "name1@123.de"));
+        mates.add(new MateObject("Name2", "name2@abc.de"));
+        mates.add(new MateObject("Name3", "name3@xyz.de"));
+    }
+
 }
